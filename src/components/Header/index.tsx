6fa7f41a8e890ec245e13c3817500364bd1446ae
Rodrigo <r.barboza9502@gmail.com>
2024-01-16 19:27:47 -0300

    Fix Header and HomeGH

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,13 @@
+import { Link } from 'react-router-dom';
 import './styles.css';
 
 export default function Header(){
     return(
         <header>
             <div className="gh-container">
+            <Link to={"/"}>
                 <h1>Github API</h1>
+            </Link>
             </div>
         </header> 
     );
--- src/routes/Home/HomeGH/index.tsx
+++ src/routes/Home/HomeGH/index.tsx
@@ -7,8 +7,8 @@ export default function HomeGH(){
         <main>
           <section>
               <div className="mt30 mb30 text-home">
-                <h1>Desafio Github API</h1>
-                <p>DevSuperior - Escola de programação</p>
+                <h1> Github API</h1>
+                <p>Encontre aqui seu perfil no Github</p>
                 <div className="mb30 mt30 dflex">
                     <Link to={"/findProfile"}>
                       <Button text="Começar" />
